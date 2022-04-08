03b89ade32085d0590055da55b335252e72a4403
Daniel23-stack <qhobosheanebokhabane@gmail.com>
2022-04-08 19:25:05 +0200

    users 4

---
+++ routes/users.js
@@ -0,0 +1,39 @@
+const {User} = require('../models/user');
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+router.get(`/`, async (req, res) =>{
+    const userList = await User.find().select('-passwordHash');
+
+    if(!userList) {
+        res.status(500).json({success: false})
+    }
+    res.send(userList);
+})
+
+router.get('/:id', async(req,res)=>{
+    const user = await User.findById(req.params.id).select('-passwordHash');
+
+    if(!user) {
+        res.status(500).json({message: 'The user with the given ID was not found.'})
+    }
+    res.status(200).send(user);
+})
+
+router.post('/', async (req,res)=>{
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+
+    })
+    user = await user.save();
+
+    if(!user)
+        return res.status(400).send('the user cannot be created!')
+
+    res.send(user);
